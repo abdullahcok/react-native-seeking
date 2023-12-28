ae859f467c7393b6ded17753fe7dd4de981a9309
Abdullah <abdullahcok@yandex.com>
2023-12-28 23:07:43 +0300

    design gon' come

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+const days = [1,2,3,4,5,6]
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text style={styles.wassup}>Wassup?</Text>
-      <Text>Fine. What about you?</Text>
-      <StatusBar style="auto" />
+      <FlatList 
+        data={days}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+        renderItem={({item}) => (
+          <View style={styles.box}>
+            <Text style={styles.text}>{item}</Text>
+          </View>
+        )}
+      />
+
+      {/* {days.map((day) => (
+        <View style={styles.box} key={day}>
+          <Text style={styles.text}>{day}</Text>
+        </View>
+      ))} */}
+
     </View>
+
   );
 }
 
@@ -15,13 +34,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+
+  content: {
+    top: 45,
+    gap: 10,
+    margin: 10
+  },
+
+  column: {
+    gap: 10,
+  },
+
+  box:{
+    backgroundColor: '#f9ede3',
+    // width: 100,
+    // height: 100,
+    flex: 1,
+    aspectRatio: 1/1.3,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#9b4521',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-  wassup:{
-    color: '#fff',
-    backgroundColor: '#2d2d2d',
-    padding: 10,
-    borderRadius: 50
-  },
+  text:{
+    color: '#9b4521',
+    fontSize: 50,
+  }
 });
